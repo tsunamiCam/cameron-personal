d994a0f93900b9f6096abb07be1e1013885fc33f
Cameron Tarbotton <tarbotto@Camerons-iMac.local>
2015-11-14 21:09:38 +1100

    Created new cleaner JS for controlling sticky navbar.

---
+++ js/sticky-navbar.js
@@ -0,0 +1,20 @@
+/* Implemetation of the sticky navigation bar functionality */
+
+$(document).ready(function() {
+
+  var  mn = $(".navbar"),
+      mns = "has-docked-nav",
+      navOffsetTop = $('nav').offset().top;
+
+  $(window).scroll(function() {
+
+    if( navOffsetTop< $(this).scrollTop() && !$('body').hasClass('has-docked-nav')) {
+      $('body').addClass(mns);
+
+    } 
+      if(navOffsetTop  > $(this).scrollTop() && $('body').hasClass('has-docked-nav')) {
+       $('body').removeClass(mns);
+    }
+  });
+
+});
