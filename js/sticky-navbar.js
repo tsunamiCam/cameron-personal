57d32a6ca25ba4da930b501e266d5908fbed03ae
Cameron Tarbotton <tarbotto@Camerons-iMac.local>
2015-11-14 23:57:30 +1100

    Add init so navbar is docked on default page before first scroll

--- js/sticky-navbar.js
+++ js/sticky-navbar.js
@@ -6,9 +6,18 @@ $(document).ready(function() {
       mns = "has-docked-nav",
       navOffsetTop = $('nav').offset().top;
 
+    if( navOffsetTop<= $(this).scrollTop() && !$('body').hasClass('has-docked-nav')) {
+      $('body').addClass(mns);
+
+    } 
+      if(navOffsetTop  > $(this).scrollTop() && $('body').hasClass('has-docked-nav')) {
+       $('body').removeClass(mns);
+    }
+
   $(window).scroll(function() {
 
-    if( navOffsetTop< $(this).scrollTop() && !$('body').hasClass('has-docked-nav')) {
+   
+    if( navOffsetTop<= $(this).scrollTop() && !$('body').hasClass('has-docked-nav')) {
       $('body').addClass(mns);
 
     } 
